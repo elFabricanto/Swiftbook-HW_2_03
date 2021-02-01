25a41a31940189b96d0ce93bd1707c1e832aee3f
Chupin Daniil <chupindaniil@gmail.com>
2021-02-01 12:46:50 +0700

    Version 1 HW 2.2

--- HW_2_03/ViewController.swift
+++ HW_2_03/ViewController.swift
@@ -8,12 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    // MARK: - IB outlets
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    @IBOutlet var colorView: UIView!
+    
+    @IBOutlet var redNumber: UILabel!
+    @IBOutlet var greenNumber: UILabel!
+    @IBOutlet var blueNumber: UILabel!
+    
+    // MARK: - Life Cycles Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redNumber.text = String(redSlider.value)
+        greenNumber.text = String(greenSlider.value)
+        blueNumber.text = String(blueSlider.value)
     }
-
-
+    
+    // MARK: - IB actions
+    @IBAction func changeRedColor() {
+        redNumber.text = String(format: "%.2f", redSlider.value)
+        colorView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1.00)
+    }
+    @IBAction func changeGreenColor() {
+        greenNumber.text = String(format: "%.2f", greenSlider.value)
+        colorView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1.00)
+    }
+    @IBAction func changeBlueColor() {
+        blueNumber.text = String(format: "%.2f", blueSlider.value)
+        colorView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1.00)
+    }
+    
 }
-
